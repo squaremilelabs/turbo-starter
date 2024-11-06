c28a6c19d51b3b789559a19da78fb350ebd1e2e6
E <e@elzr.me>
2024-11-06 15:59:09 -0500

    implement formik and restyle example page

--- apps/app-example/src/app/page.tsx
+++ apps/app-example/src/app/page.tsx
@@ -1,64 +1,93 @@
 "use client"
+
 import { Button, Card, CardBody, Input } from "@repo/ui/components/nextui"
-import { useState } from "react"
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import { useCreateItem, useFindManyItem } from "@repo/database/hooks"
+import { useFormik } from "formik"
+import { ItemCreateScalarSchema } from "@repo/database/zod"
+import { toFormikValidate } from "zod-formik-adapter"
+import { useState } from "react"
 
 export default function Page(): JSX.Element {
   const itemsQuery = useFindManyItem()
 
-  const [input, setInput] = useState("")
-  const createItemMutation = useCreateItem()
-  const onCreateItem = (): void => {
-    createItemMutation.mutate({ data: { title: input } })
-    setInput("")
-  }
+  const [mutationErrorMessage, setMutationErrorMessage] = useState<string | null>(null)
+
+  const createItemMutation = useCreateItem({
+    onError: (error) => {
+      if (error.info) {
+        const info = error.info as { message?: string }
+        if (info.message) {
+          setMutationErrorMessage(info.message)
+        }
+      } else {
+        setMutationErrorMessage(error.message)
+      }
+    },
+    onSuccess: () => {
+      formik.resetForm()
+    },
+  })
+
+  const formik = useFormik({
+    initialValues: { title: "" },
+    validate: toFormikValidate(ItemCreateScalarSchema),
+    onSubmit: (values) => {
+      createItemMutation.mutate({ data: values })
+    },
+  })
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-8">
-      <SignedOut>
-        <SignInButton>
-          <Button color="primary">Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton>
-          <Button color="default" variant="bordered">
-            Sign out
-          </Button>
-        </SignOutButton>
-      </SignedIn>
-      <Card
-        as="form"
-        classNames={{ base: "w-[500px]" }}
-        onSubmit={(e) => {
-          e.preventDefault()
-          onCreateItem()
-        }}
-        shadow="sm"
-      >
-        <CardBody className="gap-2">
-          <Input
-            color="primary"
-            label="New Item"
-            onValueChange={setInput}
-            value={input}
-            variant="bordered"
-          />
+    <main className="flex h-full w-7/12 flex-col items-stretch gap-8 self-center p-16">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold">App Example</h1>
+        <SignedIn>
+          <UserButton showName />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>
+            <Button variant="flat">Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+      <form className="flex flex-col space-y-2" onSubmit={formik.handleSubmit}>
+        <div className="flex items-center space-x-2">
+          <Input {...formik.getFieldProps("title")} label="New Item" />
           <Button
-            color="primary"
-            isDisabled={!input}
+            className="h-full"
+            isDisabled={!formik.isValid}
             isLoading={createItemMutation.isPending}
             type="submit"
           >
-            Add Item
+            Add
           </Button>
-          {createItemMutation.isError ? <p className="text-danger">There was an error</p> : null}
-        </CardBody>
-      </Card>
-      <p className="mt-4 max-w-[500px] text-center">
-        {(itemsQuery.data?.map((item) => item.title) ?? []).join(", ")}
-      </p>
-    </div>
+        </div>
+        <div>
+          {formik.errors.title && formik.values.title ? (
+            <p className="text-sm text-danger">{formik.errors.title}</p>
+          ) : null}
+          {mutationErrorMessage ? (
+            <p className="text-sm text-danger">{mutationErrorMessage}</p>
+          ) : null}
+        </div>
+      </form>
+      <div className="w-full space-y-4">
+        <h2 className="text-lg font-semibold text-default">Items</h2>
+        <div className="grid grid-cols-3 gap-4">
+          {itemsQuery.isLoading ? <p>Loading...</p> : null}
+          {itemsQuery.isError ? (
+            <p>Error: {itemsQuery.error.message}</p>
+          ) : (
+            itemsQuery.data?.map((item) => (
+              <Card isHoverable key={item.id} shadow="sm">
+                <CardBody>
+                  <p>{item.title}</p>
+                </CardBody>
+              </Card>
+            ))
+          )}
+        </div>
+      </div>
+    </main>
   )
 }
--- packages/database/src/api-handler.ts
+++ packages/database/src/api-handler.ts
@@ -1,14 +1,8 @@
-/* eslint-disable no-console -- allow logs for api handler */
-
 import { NextRequestHandler } from "@zenstackhq/server/next"
 import { prismaEnhanced } from "./prisma-enhanced"
 
 export const databaseApiHandler = NextRequestHandler({
   getPrisma: async () => prismaEnhanced(),
-  logger: {
-    error: console.error,
-    info: console.info,
-    warn: console.warn,
-  },
   useAppDir: true,
+  zodSchemas: true,
 })
