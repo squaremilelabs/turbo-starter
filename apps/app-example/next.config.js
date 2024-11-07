ebe96027c1a912a3bd6579c9361271a725707043
E <e@elzr.me>
2024-11-07 14:22:26 -0500

    add prisma monorepo workaround plugin

--- apps/app-example/next.config.js
+++ apps/app-example/next.config.js
@@ -1,5 +1,13 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin")
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
 }
