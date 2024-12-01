45ac58f9a23c60d99701e905975d3ac4af0d72da
E <e@elzr.me>
2024-12-01 14:40:55 -0500

    small lint updates

--- packages/ui/src/components/atoms/input.tsx
+++ packages/ui/src/components/atoms/input.tsx
@@ -4,6 +4,6 @@ import { Input as NextUIInput, extendVariants } from "@nextui-org/react"
 
 export const Input = extendVariants(NextUIInput, {
   defaultVariants: {
-    color: "primary",
+    variant: "bordered",
   },
 })
